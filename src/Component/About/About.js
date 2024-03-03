597aef1b25271bde8b11bb53ab3a9731da2ca52c
ABHINAY-31 <rsingh9766@gmail.com>
2024-03-03 15:33:11 +0530

    Nav bar task is done added the responsive ness to the home component

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -3,8 +3,15 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      
-
+      <div style={{display:"flex",flexDirection:"column",border:"2px solid red", height:"calc(100vh - 90px)",padding:"3px"}}>
+      <div style={{height:"50%", border:"2px solid blue"}}>1</div>
+      <div style={{border:"2px solid black", flex:"1"}}>
+        <div style={{border:"2px solid green", height:"100%", paddingTop:"10px", background:"green"}}border>
+          
+        </div>
+      </div>
+    </div>
+    
     </div>
   )
 }
--- src/Component/Nav/NavBar.js
+++ src/Component/Nav/NavBar.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import './NavBar.css'
 export default function NavBar() {
+  const[myclass, setClass] = useState("new-nav-option");
+  const[display, setDisplay] = useState("none");
+  const myFunc = () => {
+    console.log("done");
+    let l = myclass === "new-nav-option" ? "nav-options" : "new-nav-option";
+    let b = display === "none" ? "block" : "none";
+    setClass(() => l);
+    setDisplay(() => b);
+  }
   return (
     <div className="nav-bar-body">
         <div className="text">
             <p> <b>Abhinay</b>  Singh</p>
         </div>
-        <div className="nav-options">
-            <NavLink to="/" activeClassName="active">Home</NavLink>
-            <NavLink to="/About" activeClassName="active">About</NavLink>
-            <NavLink to="/Project" activeClassName="active">Project</NavLink>
-            <NavLink to="/Other" activeClassName="active">Other</NavLink>
+        <div className= {myclass} >
+            <NavLink to="/" activeClassName="active" style={{display:{display}}}>Home</NavLink>
+            <NavLink to="/About" activeClassName="active" style={{display:{display}}}>About</NavLink>
+            <NavLink to="/Project" activeClassName="active" style={{display:{display}}}>Project</NavLink>
+            <NavLink to="/Other" activeClassName="active" style={{display:{display}}}>Other</NavLink>
         </div>
+        <div className="click" onClick={myFunc}>click</div>
     </div>
   )
 }
