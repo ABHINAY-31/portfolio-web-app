c944a286626af5ab93b0ecf5f205596502f52fe4
ABHINAY-31 <rsingh9766@gmail.com>
2024-03-03 18:15:57 +0530

    Half of About Section is done

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,17 +1,86 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
+import wave from '../Images/1523.jpg'
+import backgroundWave from "../Images/rm222batch3-mind-05.jpg";
 
 export default function About() {
   return (
     <div>
-      <div style={{display:"flex",flexDirection:"column",border:"2px solid red", height:"calc(100vh - 90px)",padding:"3px"}}>
-      <div style={{height:"50%", border:"2px solid blue"}}>1</div>
-      <div style={{border:"2px solid black", flex:"1"}}>
-        <div style={{border:"2px solid green", height:"100%", paddingTop:"10px", background:"green"}}border>
-          
+      <div
+        style={{
+          minHeight: "calc(100vh - 90px)",
+          display: "flex",
+          flexDirection: "column",
+          padding: "5px",
+          background: `url(${wave})`,
+          backgroundSize:"cover",
+          backgroundRepeat:"no-repeat"
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>EDUCATION</h1>
+        <div className="cards">
+          <div className="card">
+            <img src="" alt="" />
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "end",
+                position: "relative",
+                bottom: "-100px",
+              }}
+            >
+              <p>
+                Chandigarh University <br />
+                Branch: BE <br />
+                Course: CSE <br />
+                CGPA : 8.62
+              </p>
+            </div>
+          </div>
+          <div className="card card1">
+          <img src="" alt="" />
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "end",
+                position: "relative",
+                bottom: "-100px",
+              }}
+            >
+              <p>
+                Chandigarh University <br />
+                Branch: BE <br />
+                Course: CSE <br />
+                CGPA : 8.62
+              </p>
+            </div>
+          </div>
+          <div className="card card2">
+          <img src="" alt="" />
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "end",
+                position: "relative",
+                bottom: "-100px",
+              }}
+            >
+              <p>
+                Chandigarh University <br />
+                Branch: BE <br />
+                Course: CSE <br />
+                CGPA : 8.62
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    
-    </div>
-  )
+  );
 }
